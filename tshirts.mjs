951646947090decb5135f0d7b2450e80964f96e8
manikaaggarwal <manikaaggarwal28@gmail.com>
2025-05-15 16:32:00 +0530

    Add 2 more tshirt tests

--- tshirts.mjs
+++ tshirts.mjs
@@ -13,4 +13,8 @@ function size(cms) {
 expect(size(37)).equals('S');
 expect(size(40)).equals('M');
 expect(size(43)).equals('L');
+
+expect(size(38)).equals('S');
+expect(size(42)).equals('L');
+
 console.log('All is well (maybe!)');
