0f2b2b1cf17ed0d5530a91fbe5747802a38ace1c
manikaaggarwal <manikaaggarwal28@gmail.com>
2025-05-15 17:26:23 +0530

    Update functions in misaligned.mjs + additional tests

--- misaligned.mjs
+++ misaligned.mjs
@@ -1,16 +1,46 @@
 import { expect } from 'chai';
 
+const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
+const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+
+const majorColorLength = majorColors.length;
+const minorColorLength = minorColors.length;
+
+function getMajorColor(i){
+  if(i<0|| i> majorColorLength){
+    throw new Error("Invalid index for major color");
+  }
+  return majorColors[i];
+}
+
+function getMinorColor(i){
+  if(i<0|| i>5){
+    throw new Error("Invalid index for minor color");
+  }
+  return minorColors[i];
+}
+
+function getPairNo(i, j){
+  if(i<0|| i>5 || j<0||j>5){
+    throw new Error("Invalid index for minor color");
+  }
+  return i * 5 + j;
+}
+
 function print_color_map() {
-    const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
-    const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
-    for (let i = 0; i < majorColors.length; i++) {
-        for (let j = 0; j < minorColors.length; j++) {
-            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
+    for (let i = 0; i < majorColorLength; i++) {
+        for (let j = 0; j < minorColorLength; j++) {
+            console.log(`${getPairNo(i,j)} | ${getMajorColor(i)} | ${getMinorColor(j)}`);
         }
     }
-    return majorColors.length * minorColors.length;
+    return majorColorLength * minorColorLength;
 }
 
 const result = print_color_map();
 expect(result).equals(25);
+expect(getMajorColor(0)).equals("White");
+expect(getMinorColor(1)).equals("Blue");
+expect(getPairNo(0,0)).equals(1);
+
+
 console.log('All is well (maybe!)');
